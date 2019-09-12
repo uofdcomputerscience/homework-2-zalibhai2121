9805ae5ed383ff7cca15e0067c0e7a89b4fa5609
Russell Mirabelli <russell@mirabellifamily.org>
2019-09-12 08:10:08 -0500

    Initial Commit

---
+++ SecondProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondProject
+//
+//  Created by Russell Mirabelli on 9/12/19.
+//  Copyright © 2019 Russell Mirabelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
